9bbab8ed1ac4e73238b12ca6b7121a9830dc32aa
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-05-21 22:58:30 +0530

    deleted:    src/app/client-greeting.tsx 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,11 @@
-"use server";
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { ErrorBoundary } from "react-error-boundary";
-import React, { Suspense } from "react";
-import { ClientGreeting } from "./client-greeting";
+import React from 'react'
 
-const HomePage = async () => {
-  const queryClient = getQueryClient();
-
-  void (await queryClient.prefetchQuery(
-    trpc.hello.queryOptions({
-      text: "Hello from TRPC",
-    })
-  ));
-
+const HomePage = () => {
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ErrorBoundary fallback={<div>Error</div>}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <ClientGreeting />
-        </Suspense>
-      </ErrorBoundary>
-    </HydrationBoundary>
-  );
-};
+    <div>
+      Home Page
+    </div>
+  )
+}
 
-export default HomePage;
+export default HomePage
