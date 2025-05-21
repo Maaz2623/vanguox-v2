6b8df75c26361f8abdcf1e98255fc9029be05ca2
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-05-21 21:45:40 +0530

    deleted:    .gitignore 	deleted:    README.md 	deleted:    auth-schema.ts 	deleted:    bun.lock 	deleted:    components.json 	deleted:    drizzle.config.ts 	deleted:    eslint.config.mjs 	deleted:    next.config.ts 	deleted:    package.json 	deleted:    postcss.config.mjs 	deleted:    public/file.svg 	deleted:    public/globe.svg 	deleted:    public/headphones.jpg 	deleted:    public/logo.svg 	deleted:    public/next.svg 	deleted:    public/vercel.svg 	deleted:    public/window.svg 	deleted:    src/app/(main)/page.tsx 	deleted:    src/app/(main)/stores/[storeName]/page.tsx 	deleted:    src/app/api/auth/[...all]/route.ts 	deleted:    src/app/api/trpc/[trpc]/route.ts 	deleted:    src/app/auth/layout.tsx 	deleted:    src/app/auth/sign-in/page.tsx 	deleted:    src/app/favicon.ico 	deleted:    src/app/globals.css 	deleted:    src/app/layout.tsx 	deleted:    src/auth.ts 	deleted:    src/components/search-bar.tsx 	deleted:    src/components/ui/accordion.tsx 	deleted:    src/components/ui/alert-dialog.tsx 	deleted:    src/components/ui/alert.tsx 	deleted:    src/components/ui/aspect-ratio.tsx 	deleted:    src/components/ui/avatar.tsx 	deleted:    src/components/ui/badge.tsx 	deleted:    src/components/ui/breadcrumb.tsx 	deleted:    src/components/ui/button.tsx 	deleted:    src/components/ui/calendar.tsx 	deleted:    src/components/ui/card.tsx 	deleted:    src/components/ui/carousel.tsx 	deleted:    src/components/ui/chart.tsx 	deleted:    src/components/ui/checkbox.tsx 	deleted:    src/components/ui/collapsible.tsx 	deleted:    src/components/ui/command.tsx 	deleted:    src/components/ui/context-menu.tsx 	deleted:    src/components/ui/dialog.tsx 	deleted:    src/components/ui/drawer.tsx 	deleted:    src/components/ui/dropdown-menu.tsx 	deleted:    src/components/ui/form.tsx 	deleted:    src/components/ui/hover-card.tsx 	deleted:    src/components/ui/input-otp.tsx 	deleted:    src/components/ui/input.tsx 	deleted:    src/components/ui/label.tsx 	deleted:    src/components/ui/menubar.tsx 	deleted:    src/components/ui/navigation-menu.tsx 	deleted:    src/components/ui/pagination.tsx 	deleted:    src/components/ui/popover.tsx 	deleted:    src/components/ui/progress.tsx 	deleted:    src/components/ui/radio-group.tsx 	deleted:    src/components/ui/resizable.tsx 	deleted:    src/components/ui/scroll-area.tsx 	deleted:    src/components/ui/select.tsx 	deleted:    src/components/ui/separator.tsx 	deleted:    src/components/ui/sheet.tsx 	deleted:    src/components/ui/sidebar.tsx 	deleted:    src/components/ui/skeleton.tsx 	deleted:    src/components/ui/slider.tsx 	deleted:    src/components/ui/sonner.tsx 	deleted:    src/components/ui/switch.tsx 	deleted:    src/components/ui/table.tsx 	deleted:    src/components/ui/tabs.tsx 	deleted:    src/components/ui/textarea.tsx 	deleted:    src/components/ui/toggle-group.tsx 	deleted:    src/components/ui/toggle.tsx 	deleted:    src/components/ui/tooltip.tsx 	deleted:    src/db/index.ts 	deleted:    src/db/schema.ts 	deleted:    src/helpers/get-subdomain.ts 	deleted:    src/hooks/use-mobile.ts 	deleted:    src/lib/auth-client.ts 	deleted:    src/lib/utils.ts 	deleted:    src/middleware.ts 	deleted:    src/modules/vanguox/_components/navbar/index.tsx 	deleted:    src/modules/vanguox/_components/navbar/nav-store-button.tsx 	deleted:    src/modules/vanguox/_components/navbar/nav-user-button.tsx 	deleted:    src/modules/vanguox/vanguox-layout.tsx 	deleted:    src/modules/vanguox/vanguox-page.tsx 	deleted:    src/trpc/client.tsx 	deleted:    src/trpc/init.ts 	deleted:    src/trpc/procedures/stores.procedure.ts 	deleted:    src/trpc/query-client.ts 	deleted:    src/trpc/routers/_app.ts 	deleted:    src/trpc/server.tsx 	deleted:    tsconfig.json

--- src/app/(main)/stores/[storeName]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const StoreName = () => {
-  return <div>Store Name</div>;
-};
-
-export default StoreName;
--- src/trpc/routers/_app.ts
+++
@@ -1,7 +0,0 @@
-import { createTRPCRouter } from "../init";
-import { storesRouter } from "../procedures/stores.procedure";
-export const appRouter = createTRPCRouter({
-  stores: storesRouter,
-});
-// export type definition of API
-export type AppRouter = typeof appRouter;
